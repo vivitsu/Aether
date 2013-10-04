3c85a9ca915b9300caad1f7e6db748a056cec7bd
Aniket Oak <aoak12@ufl.edu>
2013-10-04 16:18:40 -0400

    separation of conf and core

--- src/aether/core/AetherNode.java
+++ src/aether/core/AetherNode.java
@@ -4,8 +4,7 @@
  */
 package aether.core;
 
-import aether.net.*;
-import java.util.HashMap;
+import aether.conf.*;
 
 /**
  *
@@ -14,39 +13,22 @@ import java.util.HashMap;
 public class AetherNode {
     
     
-    private static HashMap<String,String> configDict;
     
     
     
-    /**
-     * Initialize the configuration key value store by parsing the configuration
-     * file.
-     * @param file Name of the configuration file. Can be null for default
-     * @return void. Exits if parsing fails.
-     */
-    public static void initConfig(String file) {
-        /* We can do nothing till the time we parse the configuration file,
-         * so do that first.
-         */
-        ConfigParser parser;
-        
-        if (file != null) {
-            parser = new ConfigParser(file);
-        } else {
-            parser = new ConfigParser();
-        }
-        
-        configDict = parser.populate();
-        
-        if (configDict == null) {
-            System.exit(1);
-        } 
-    }
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     public static void main (String[] argv) {
         
         String configFile = (argv.length == 0) ? null : argv[0];
-        initConfig(configFile);
+        ConfigMgr.initConfig(configFile);
     }
 }
