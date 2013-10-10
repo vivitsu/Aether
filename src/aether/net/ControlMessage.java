1df011ae202de0d0debb62a316f3ee743e7b3378
Aniket Oak <aoak12@ufl.edu>
2013-10-10 14:22:56 -0400

    Parser for "j" message

--- src/aether/net/ControlMessage.java
+++ src/aether/net/ControlMessage.java
@@ -1,7 +1,9 @@
 
 package aether.net;
 
+import aether.cluster.ClusterTableRecord;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -34,4 +36,37 @@ public class ControlMessage extends Message {
     }
     
     
+    
+    /**
+     * Parse the control message with subtype 'j' to create an array of cluster
+     * table records from the payload.
+     * @return  Array of cluster table records
+     * @throws UnknownHostException if we fail to create IP address from string
+     * @throws UnsupportedOperationException If the message type is not 'j'
+     */
+    public ClusterTableRecord[] parseJControl () throws UnknownHostException,
+            UnsupportedOperationException {
+        
+        if (header.getSubtype() != 'j') {
+            throw new UnsupportedOperationException();
+        }
+        
+        ClusterTableRecord[] recArray = new ClusterTableRecord[0];
+        char[] data = payload.getData();
+        
+        String tableString = data.toString();
+        /* Separate the hashtable records by spliting on the record separator */
+        String[] records = tableString.split(";");
+        
+        for (String record: records) {
+            /* Split the record to separate key and values */
+            String[] tokens = record.split("%");
+            int id = Integer.parseInt(tokens[0]);
+            InetAddress ip = InetAddress.getByName(tokens[1]);
+            ClusterTableRecord rec = new ClusterTableRecord (id, ip);
+            recArray[recArray.length] = rec;
+        }
+        
+        return recArray;
+    }
 }
