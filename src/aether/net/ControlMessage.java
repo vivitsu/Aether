ab0a505ea5cd9e96426f82bf604d67eb90d51ded
Aniket Oak <aoak12@ufl.edu>
2013-11-15 15:45:49 -0500

    documentation. parse p, c, a control. New constructor

--- src/aether/net/ControlMessage.java
+++ src/aether/net/ControlMessage.java
@@ -7,6 +7,41 @@ import java.net.UnknownHostException;
 
 /**
  *
+ * Control messages are sent from one node in the cluster to the other. The
+ * purpose of the control message is communicating the cluster management and 
+ * other meta data between the nodes of the cluster. Control messages have 
+ * different subtypes which signify different purpose of the control message.
+ * Control message inherits from class Message which has header and payload.
+ * 
+ * An important field in header of the control message is its subtype which
+ * can be one of the following:
+ * 
+ * 'd': Discovery message:
+ *      This message is sent by a new node who wants to join the cluster. All
+ *      the nodes in the cluster should respond to this message by discovery
+ *      response message of type 'r'.
+ * 'r': Discovery response message:
+ *      This message is sent by each node in the cluster to the new node who
+ *      sent a discovery message.
+ * 'm': Membership request message:
+ *      This message is sent by the new node wishing the membership of the 
+ *      cluster to one of the nodes that sent it the discovery response 'r'.
+ * 'p': Membership proposal message:
+ *      The node in the cluster who receives the membership request 'm', sends 
+ *      a membership proposal to all the nodes in the cluster. In the payload,
+ *      it has the proposed id of the new node
+ * 'a': Proposal response message:
+ *      Other nodes in the cluster respond to the proposal with proposal 
+ *      response message. In the payload, it has 'accept' or 'deny' reply.
+ * 'c': Membership commit message:
+ *      Once the contact node receives acceptance from all the nodes, it sends
+ *      the membership commit message telling all the nodes to commit the  new
+ *      node to their cluster tables.
+ * 'j': Join message:
+ *      After sending the commit message, the contact node sends a join message
+ *      to the new node. In the payload, it has the list of nodes already 
+ *      present in the cluster.
+ * 
  * @author aniket
  */
 public class ControlMessage extends Message {
@@ -24,6 +59,15 @@ public class ControlMessage extends Message {
         
         super ('c', subtype, dest);
     }
+    
+    
+    
+    public ControlMessage (char subtype, InetAddress dest, char[] payload) {
+        
+        super('c', payload, subtype, dest);
+    }
+    
+    
     
     
     /**
@@ -68,5 +112,48 @@ public class ControlMessage extends Message {
         }
         
         return recArray;
+    }
+    
+    
+    
+    
+    /**
+     * Parse the control message with subtype 'a' to return the string in its 
+     * payload
+     * @return  Payload in string form
+     */
+    public String parseAControl () {
+        
+        return payload.getData().toString();
+    }
+    
+    
+    
+    /**
+     * Parse the control message with subtype 'p' to return the cluster table
+     * record for the new node
+     * @return  Cluster table record for new node
+     * @throws UnknownHostException
+     */
+    public ClusterTableRecord parsePControl () throws UnknownHostException {
+        
+        String newNodeRecString = payload.getData().toString();
+        String[] tokens = newNodeRecString.split("%");
+        int id = Integer.parseInt(tokens[0]);
+        InetAddress ip = InetAddress.getByName(tokens[1]);
+        ClusterTableRecord rec = new ClusterTableRecord (id, ip);
+        return rec;    
+    }
+    
+    
+    
+    /**
+     * Parse the control message with subtype 'c' to return the node id for
+     * which commit confirm is received
+     * @return  Integer new node id
+     */
+    public Integer parseCControl () {
+        
+        return Integer.parseInt(payload.toString());
     }
 }
