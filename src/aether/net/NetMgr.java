fd16301a2db418b785625dae3243349b30d35ea8
Aniket Oak <aoak12@ufl.edu>
2013-12-01 16:42:10 -0500

    Fixed problem with serialization. Enabled broadcast. Ignore messages from self

--- src/aether/net/NetMgr.java
+++ src/aether/net/NetMgr.java
@@ -5,7 +5,7 @@
 package aether.net;
 
 import aether.conf.ConfigMgr;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -17,6 +17,12 @@ import java.net.InetAddress;
 import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  *
@@ -27,6 +33,8 @@ public class NetMgr {
     
     
     private static String interfaceName = ConfigMgr.getInterfaceName();
+    private static final Logger log = Logger.getLogger(NetMgr.class.getName());
+    
     
     
     
@@ -34,10 +42,29 @@ public class NetMgr {
     private DatagramSocket socket;
     
     
+    static {
+        log.setLevel(Level.INFO);
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setFormatter(new SimpleFormatter());
+        handler.setLevel(Level.ALL);
+        log.addHandler(handler);
+    }
+    
+    
     public NetMgr (int port) throws SocketException {
         
         this.port = port;
-        socket = new DatagramSocket (port, ConfigMgr.getLocalIp());
+        socket = new DatagramSocket (port);
+        
+        if (ConfigMgr.getIfDebug()) {
+                log.setLevel(Level.FINE);
+            }
+        
+        socket.setBroadcast(true);
+        if (socket.getBroadcast()) {
+            log.fine("Enabled broadcast reception");
+        }
+        
     }
     
     
@@ -51,6 +78,7 @@ public class NetMgr {
      * @throws SocketException 
      */
     public void setTimeout (int timeout) throws SocketException {
+        log.log(Level.FINE,"Setting timeout to {0} ms", timeout);
         socket.setSoTimeout(timeout);
     }
     
@@ -69,7 +97,10 @@ public class NetMgr {
         ByteArrayOutputStream bOut = new ByteArrayOutputStream();
         ObjectOutputStream oOut = new ObjectOutputStream(bOut);
         oOut.writeObject(m);
-        return bOut.toByteArray();
+        byte[] d = bOut.toByteArray();
+        log.log(Level.FINER,"Serialized the message to {0} bytes", 
+                d.length);
+        return d;
     }
     
     
@@ -84,6 +115,10 @@ public class NetMgr {
         DatagramPacket packet = new DatagramPacket(datagramPayload, 
                 datagramPayload.length, m.getDestIp(), port);
         
+        
+        log.log(Level.INFO, "Sending {0} bytes to node {1} port: {2}", 
+                new Object[]{ datagramPayload.length, m.getDestIp().toString(), 
+                    port});
         socket.send(packet);
     }
     
@@ -95,14 +130,33 @@ public class NetMgr {
      * @throws IOException 
      */
     public Message receive () throws IOException {
-        DatagramPacket dResponse = new DatagramPacket(new byte[1024],1024);
-        socket.receive(dResponse);
+        
+        DatagramPacket dResponse = null;
+        for (int i = 0; i < 5; i++) {
+
+            dResponse = new DatagramPacket(new byte[10000], 10000);
+            socket.receive(dResponse);
+
+            if (dResponse.getAddress().equals(getLocalIp())) {
+                if (i == 5) {
+                    throw new SocketTimeoutException();
+                }
+            } else {
+                break;
+            }
+        }
+        
         Message m = null;
         try {
-            m = deserializeMessage(dResponse.getData());
+            log.log(Level.FINE, "Recceived {0} bytes from node {1}", 
+                    new Object[]{dResponse.getLength(),
+                        dResponse.getSocketAddress().toString()});
+            
+            byte[] dataCopy = Arrays.copyOf(dResponse.getData(), 
+                        dResponse.getLength());
+            m = deserializeMessage(dataCopy);
         } catch (ClassNotFoundException ex) {
-            System.err.println("[ERROR]: Retrieving message from datagram"
-                    + " class not found");
+            log.warning("Error retrieving message from the datagram");
             ex.printStackTrace();
         }
         return m;
@@ -120,6 +174,8 @@ public class NetMgr {
     private Message deserializeMessage (byte[] bytes) throws IOException, 
             ClassNotFoundException {
         Message m;
+        log.log(Level.FINE, "Deserialized {0} bytes to extract a message",
+                bytes.length);
         m = (Message) deserialize (bytes);
         return m;
     }
@@ -134,7 +190,7 @@ public class NetMgr {
      */
     private Object deserialize (byte[] bytes) throws IOException, 
             ClassNotFoundException {
-        ByteInputStream bIn = new ByteInputStream();
+        ByteArrayInputStream bIn = new ByteArrayInputStream(bytes);
         ObjectInputStream oIn = new ObjectInputStream(bIn);
         return oIn.readObject();
     }
@@ -155,7 +211,7 @@ public class NetMgr {
                 return null;
             }
         } catch (SocketException e) {
-            System.err.println("[ERROR]: could not find the interface");
+            log.warning("Could not find the interface");
             return null;
         }
         
@@ -175,6 +231,11 @@ public class NetMgr {
     }
     
     
+    /**
+     * Get local IPv4 address of this machine
+     * @return  InetAddress IPv4 address of the interface given in the 
+     *          configuration file
+     */
     public static InetAddress getLocalIp () {
         
         NetworkInterface iFace;
@@ -184,7 +245,7 @@ public class NetMgr {
                 return null;
             }
         } catch (SocketException e) {
-            System.err.println("[ERROR]: could not find the interface");
+            log.warning("Could not find the interface");
             return null;
         }
         
