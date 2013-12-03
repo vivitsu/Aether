d9569481fc4c14ed3efcda4c506e4751cf442c8e
Devesh Gade <deveshgade152003@gmail.com>
2013-12-02 23:14:47 -0500

    Implemented Heartbeat in the replication daemon
    -Implemented Heartbeat, heartbeat buddy map
    -used for fault tolerance, keeping track of which nodes in the cluster
    are alive and which are failed

---
+++ src/aether/repl/Replication.java
@@ -0,0 +1,211 @@
+package aether.repl;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Replication {
+	public static int HTBT_SND_PORT_NUMBER = 34444;
+	public static int HTBT_RCV_PORT_NUMBER = 34445;
+	public static int NUM_RETRIES = 4;
+	public static int TIMEOUT_BEFORE_DEAD = 10000;	//milliseconds
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HtbtBuddyMap hbm = new HtbtBuddyMap ();
+		try {
+			hbm.put(new Host (InetAddress.getLocalHost(), Replication.HTBT_RCV_PORT_NUMBER), null);
+			System.out.println(InetAddress.getLocalHost());
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		HtbtSender s = new HtbtSender (hbm);
+		new Thread(s).start();
+		
+		HtbtReceiver r = new HtbtReceiver();
+		new Thread(r).start();
+
+		
+	}
+}
+class HtbtBuddyMap implements Runnable {
+	Map<Host, HostDetails> clusterNodes;
+	public HtbtBuddyMap () {
+		clusterNodes = new HashMap<Host, HostDetails> ();
+	}
+	public Set<Map.Entry<Host, HostDetails>> getBuddyMapSet () {
+		return clusterNodes.entrySet();
+	}
+	public void put (Host h, HostDetails hd) {
+		clusterNodes.put(h, hd);
+	}
+	public HostDetails get (Host h) {
+		return clusterNodes.get(h);
+	}
+	public void run() {
+		while (true) {
+			long difference;
+			for (Entry<Host, HostDetails> entry : clusterNodes.entrySet()) {
+				synchronized (entry) {
+					difference = System.currentTimeMillis() - entry.getValue().getTime();
+					if ((difference / 1000) > Replication.TIMEOUT_BEFORE_DEAD) {
+						//Replace with logic of what to do when dead
+						System.out.println("Port " + entry.getKey() + " dead");
+					}
+				}
+			}
+			try {
+				Thread.currentThread().sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	public void saveTime (Host h, HostDetails hd) {
+		HostDetails oldDetail;
+		if ( clusterNodes.get(h) != null ) {
+			oldDetail = clusterNodes.get(h);
+			System.out.println("Saving port + time: " + " " + (hd.getTime()- oldDetail.getTime()));
+		}
+		clusterNodes.put(h, hd);
+	}
+	
+}
+class HtbtSender implements Runnable {
+	HtbtBuddyMap buddymap;
+	public HtbtSender (HtbtBuddyMap hbm) {
+		buddymap = hbm;
+	}
+	public void setBuddyMap (HtbtBuddyMap hbm) {
+		buddymap = hbm;
+	}
+	public void run () {
+		DatagramSocket ds;
+		try {
+			ds = new DatagramSocket(Replication.HTBT_SND_PORT_NUMBER,
+					InetAddress.getLocalHost());
+			byte[] send = new byte[256];
+			while (true) {
+
+				for (Entry<Host, HostDetails> entry : buddymap.getBuddyMapSet()) {
+
+					String mess = "from Sender";// + Integer.toString(port);
+					send = mess.getBytes("UTF-8");
+					DatagramPacket dps = new DatagramPacket(send, send.length,
+							entry.getKey().getIPAddress(), entry.getKey().getPort());
+					ds.send(dps);
+				}
+				System.out.println("Sent from sender");
+				Thread.currentThread().sleep(5000);
+			}
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}	
+}
+class HtbtReceiver implements Runnable {
+	HtbtBuddyMap buddyMap;
+	public HtbtReceiver () {
+		buddyMap = new HtbtBuddyMap();
+		new Thread(buddyMap).start();
+	}
+	public void run () {
+		try {
+			DatagramSocket ds = new DatagramSocket(Replication.HTBT_RCV_PORT_NUMBER,
+					InetAddress.getLocalHost());				
+			byte[] receive = new byte[256];
+			DatagramPacket dpr = new DatagramPacket(receive, receive.length);
+			while (!Thread.currentThread().isInterrupted()) {
+				ds.receive(dpr);
+				buddyMap.saveTime (new Host(dpr.getAddress(),dpr.getPort()), new HostDetails (System.currentTimeMillis()));
+				String message = new String(receive, 0, dpr.getLength(),
+						"UTF-8");
+				System.out.println("S : got " + message);
+			}			
+
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//System.out.println("Retrying ");
+			//continue;
+		}
+	}
+}
+class ChunkBuddyList {
+	 ArrayList<InetAddress> list;
+	 public ArrayList getList () {
+		 return list;
+	 }
+	 public void addNode (InetAddress ia) {
+		 list.add(ia);
+	 }
+}
+class HostDetails {
+	long time;	
+	public HostDetails (long t) {
+		time = t;
+	}
+	public void setTime (long t) {
+		time = t;
+	}
+	public long  getTime () {
+		return time; 
+	}	
+}
+class Host {
+	InetAddress ipAddress;
+	int port;
+	public Host (InetAddress ia, int p) {
+		ipAddress = ia;
+		port = p;
+	}
+	public void setIPAddress (InetAddress ia) {
+		ipAddress = ia;
+	}
+	public void setPort (int p) {
+		port = p;
+	}
+	public InetAddress getIPAddress () {
+		return ipAddress;
+	}
+	public int getPort () {
+		return port;
+	}
+}
