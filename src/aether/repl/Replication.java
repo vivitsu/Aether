841208359a07c84563c8ba59d7d37c58e587cb72
Heramb <herambgadekar@ufl.edu>
2013-12-11 01:04:54 -0500

    chunkspacemap_bugfix

--- src/aether/repl/Replication.java
+++ src/aether/repl/Replication.java
@@ -32,7 +32,7 @@ public class Replication {
 		HtbtSender s = new HtbtSender (hbm);		
 		HtbtReceiver r = new HtbtReceiver ();
 		ReplicationListener rl = ReplicationListener.getInstance();
-		ChunkSpaceMap csm = new ChunkSpaceMap ();
+		ChunkSpaceMap csm = ChunkSpaceMap.getInstance();
 		ChunkManager cm = ChunkManager.getInstance();
 		FileChunkMetadata fcm = new FileChunkMetadata ();
 		new Thread(s).start();
